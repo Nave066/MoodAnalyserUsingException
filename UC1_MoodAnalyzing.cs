3033243c2906d2cb00f86f996e8135e18301b861
Naveen V <jayamnaveen68@gmail.com>
2022-02-13 13:34:25 +0530

    UC1

---
+++ UC1_MoodAnalyzing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserUsingException
+{
+    public class UC1_MoodAnalyzing
+    {
+        string message;
+        public UC1_MoodAnalyzing(string message)
+        {
+            this.message = message;
+        }
+
+        public string AnalyzeMood()
+        {
+            if (message.ToLower().Contains("happy"))
+            {
+                return "happy";
+            }
+            else
+            {
+                return "sad";
+            }
+        }
+    }
+}
